90718e77a1226fa8d549b49e3539f6974ac692a7
AliaksandraBychkouskaya <aleksandra.bychkovskaya95@gmail.com>
2018-02-24 17:27:50 +0300

    solution for Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let countOf5 = 0;
+  let countOf2 = 0;
+  let numberForChanges;
+  do {
+    numberForChanges = number;
+    while (numberForChanges > 1 && (numberForChanges % 5 === 0 || numberForChanges % 2 === 0)) {
+      if (numberForChanges % 5 === 0) {
+        numberForChanges /= 5;
+        countOf5++;
+      }
+      if (numberForChanges % 2 === 0) {
+        numberForChanges /= 2;
+        countOf2++;
+      }
+    }
+
+    number -= 1;
+  }
+  while (number > 0)
+  return Math.min(countOf2, countOf5);
 }
